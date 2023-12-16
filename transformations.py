78461d98247f7d4c1f7733ca52921dbcf82b7bb8
ShreyPatel1311 <shreybpatel@gmail.com>
2023-12-16 23:31:15 +0530

    Transformations

--- transformations.py
+++ transformations.py
@@ -5,7 +5,7 @@ def translate(img, x, y):
     transMat = np.float32([
         [1,0,x],
         [0,1,y]
-    ])  # type: ignore
+    ]) # type: ignore
     dimensions = (img.shape[1], img.shape[0])
     return cv.warpAffine(img, transMat, dimensions) # type: ignore
 
